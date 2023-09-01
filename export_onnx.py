0bbd6cf37602cc7c9cae81c2e892308e8588632b
woodwu <woodwu@lenovo.com>
2023-09-01 11:36:36 +0800

    update onnx

--- export_onnx.py
+++ export_onnx.py
@@ -41,36 +41,60 @@ class Exporter:
         if self.args.checkpoint is not None:
             self.model.load_state_dict(torch.load(self.args.checkpoint, map_location=self.args.device), strict=False)
         
+    # def export(self):
+    #     rec = (torch.zeros([1, 1, 1, 1]).to(self.args.device, self.precision),) * 4
+    #     src = torch.randn(1, 3, 1080, 1920).to(self.args.device, self.precision)
+    #     downsample_ratio = torch.tensor([0.25]).to(self.args.device)
+        
+    #     dynamic_spatial = {0: 'batch_size', 2: 'height', 3: 'width'}
+    #     dynamic_everything = {0: 'batch_size', 1: 'channels', 2: 'height', 3: 'width'}
+        
+    #     torch.onnx.export(
+    #         self.model,
+    #         (src, *rec, downsample_ratio),
+    #         self.args.output,
+    #         export_params=True,
+    #         opset_version=self.args.opset,
+    #         do_constant_folding=True,
+    #         input_names=['src', 'r1i', 'r2i', 'r3i', 'r4i', 'downsample_ratio'],
+    #         output_names=['fgr', 'pha', 'r1o', 'r2o', 'r3o', 'r4o'],
+    #         dynamic_axes={
+    #             'src': dynamic_spatial,
+    #             'fgr': dynamic_spatial,
+    #             'pha': dynamic_spatial,
+    #             'r1i': dynamic_everything,
+    #             'r2i': dynamic_everything,
+    #             'r3i': dynamic_everything,
+    #             'r4i': dynamic_everything,
+    #             'r1o': dynamic_spatial,
+    #             'r2o': dynamic_spatial,
+    #             'r3o': dynamic_spatial,
+    #             'r4o': dynamic_spatial,
+    #         })
+
     def export(self):
-        rec = (torch.zeros([1, 1, 1, 1]).to(self.args.device, self.precision),) * 4
-        src = torch.randn(1, 3, 1080, 1920).to(self.args.device, self.precision)
-        downsample_ratio = torch.tensor([0.25]).to(self.args.device)
-        
-        dynamic_spatial = {0: 'batch_size', 2: 'height', 3: 'width'}
-        dynamic_everything = {0: 'batch_size', 1: 'channels', 2: 'height', 3: 'width'}
-        
-        torch.onnx.export(
-            self.model,
-            (src, *rec, downsample_ratio),
-            self.args.output,
-            export_params=True,
-            opset_version=self.args.opset,
-            do_constant_folding=True,
-            input_names=['src', 'r1i', 'r2i', 'r3i', 'r4i', 'downsample_ratio'],
-            output_names=['fgr', 'pha', 'r1o', 'r2o', 'r3o', 'r4o'],
-            dynamic_axes={
-                'src': dynamic_spatial,
-                'fgr': dynamic_spatial,
-                'pha': dynamic_spatial,
-                'r1i': dynamic_everything,
-                'r2i': dynamic_everything,
-                'r3i': dynamic_everything,
-                'r4i': dynamic_everything,
-                'r1o': dynamic_spatial,
-                'r2o': dynamic_spatial,
-                'r3o': dynamic_spatial,
-                'r4o': dynamic_spatial,
-            })
+            # rec1 = torch.zeros([1,16,135,240]).to(self.args.device, self.precision)
+            # rec2 = torch.zeros([1,20,68,120]).to(self.args.device, self.precision)
+            # rec3 = torch.zeros([1,40,34,60]).to(self.args.device, self.precision)
+            # rec4 = torch.zeros([1,64,17,30]).to(self.args.device, self.precision)
+            # src = torch.randn([1,3,1080,1920]).to(self.args.device, self.precision)
+
+            rec1 = torch.zeros([1,16,144,256]).to(self.args.device, self.precision)
+            rec2 = torch.zeros([1,20,72,128]).to(self.args.device, self.precision)
+            rec3 = torch.zeros([1,40,36,64]).to(self.args.device, self.precision)
+            rec4 = torch.zeros([1,64,18,32]).to(self.args.device, self.precision)
+            src = torch.randn([1,3,288,512]).to(self.args.device, self.precision)
+            
+            torch.onnx.export(
+                self.model,
+                (src, rec1, rec2, rec3, rec4),
+                self.args.output,
+                export_params=True,
+                opset_version=self.args.opset,
+                do_constant_folding=True,
+                input_names=['src', 'r1i', 'r2i', 'r3i', 'r4i'],
+                output_names=['fgr', 'pha', 'r1o', 'r2o', 'r3o', 'r4o']
+                )
 
 if __name__ == '__main__':
     Exporter()
--- model/resnet.py
+++ model/resnet.py
@@ -1,6 +1,8 @@
 from torch import nn
 from torchvision.models.resnet import ResNet, Bottleneck
-from torchvision.models.utils import load_state_dict_from_url
+# from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
+
 
 class ResNet50Encoder(ResNet):
     def __init__(self, pretrained: bool = False):
